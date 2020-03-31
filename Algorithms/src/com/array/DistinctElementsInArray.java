137299f02dc58db5bc1206dda7f10ead5b949980
Prerna Gandhi <prernagandhi97@gmail.com>
2020-03-31 22:29:43 +0530

    renamed variables

--- Algorithms/src/com/array/DistinctElementsInArray.java
+++ Algorithms/src/com/array/DistinctElementsInArray.java
@@ -3,14 +3,14 @@ package com.array;
 public class DistinctElementsInArray {
 	
 	public static void main(String[] args) {
-		int arr[] = {1,2,12,12,5,3,2,13,54,6,54,6,2,4,3};
-		String s ="";
-		for(int a: arr) {
-			if(s.contains(String.valueOf(a))) {
+		int inputArray[] = {1,2,12,12,5,3,2,13,54,6,54,6,2,4,3};
+		String stringForStoringDistinctElements ="";
+		for(int number: inputArray) {
+			if(stringForStoringDistinctElements.contains(String.valueOf(number))) {
 				
 			}else {
-				System.out.println(a);
-				s= s+ a+ "-";
+				System.out.println(number);
+				stringForStoringDistinctElements= stringForStoringDistinctElements+ number+ "-";
 			}
 		}
 	}
