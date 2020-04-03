17dcb742e456250838049d71f537b5cd78c2b5eb
Prerna Gandhi <prernagandhi97@gmail.com>
2020-04-03 15:18:25 +0530

    Print Sub Array and difference between highest and lowest frequencies

---
+++ Algorithms/src/com/array/PrintSubArrays.java
@@ -0,0 +1,47 @@
+package com.array;
+
+import java.util.Stack;
+
+public class PrintSubArrays {
+
+	public static void main(String[] args) {
+		int inputArray[] = { 1, 2, 3, 4 };
+		// 1
+		// 1 2
+		// 1 2 3
+		// 1 2 3 4
+		// 1 2 4
+		// 1 4
+		// 1 3 4
+		// 1 3
+		// 2
+		// 2 3
+		// 2 3 4
+		// 2 4
+		// 3
+		// 3 4
+		// 4
+		PrintSubArrays printSubArrays = new PrintSubArrays();
+		printSubArrays.subsets(inputArray);
+	}
+
+	private void subsets(int[] inputArray) {
+		Stack<Integer> subsets = new Stack<Integer>();
+		subsetUtil(inputArray, subsets, 0);
+	}
+
+	private void subsetUtil(int[] inputArray, Stack<Integer> subsets, int i) {
+		if (subsets.isEmpty()) {
+			;
+		} else {
+			System.out.println(subsets);
+		}
+		for (int index = i; index < inputArray.length; index++) {
+			subsets.push(inputArray[index]);
+
+			subsetUtil(inputArray, subsets, index + 1);
+
+			subsets.pop();
+		}
+	}
+}
