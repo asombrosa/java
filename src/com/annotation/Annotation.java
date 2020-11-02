6837a3525f764ab5164ae7170077cccfb1155979
Prerna Gandhi <prernagandhi97@gmail.com>
2020-11-02 18:32:48 +0530

    Annotation and generic class test case

---
+++ src/com/annotation/Annotation.java
@@ -0,0 +1,29 @@
+package com.annotation;
+
+class Base {
+    public void display() {
+        System.out.println("Base display()");
+    }
+}
+
+class Derived extends Base {
+    @Override
+    public void display() {
+        System.out.println("Derived display(int )");
+    }
+
+   /* @Override
+    public void invalid(){
+        System.out.println("invalid");
+    }*/
+}
+
+public class Annotation {
+    public static void main(String args[]) {
+        Derived obj = new Derived();
+        obj.display();
+        //If you try to override display method
+        //and pass in parameters it would be compile
+        //time error
+    }
+}
