e14994ebf62fd5d89e8c16c93357874c647acd1d
Prerna Gandhi <prernagandhi97@gmail.com>
2021-02-16 23:21:45 +0530

    Microsoft Interview Question : Approach 4 using MinHeap for finding k largest elements in an array

---
+++ MicrosoftInterviewQuestions/src/com/interview/questions/sde2/kLargestElementInArray/Approach4UsingMinHeap.java
@@ -0,0 +1,36 @@
+package com.interview.questions.sde2.kLargestElementInArray;
+
+import java.util.PriorityQueue;
+
+/*
+For example, if given array is [1, 23, 12, 9, 30, 2, 50]
+and you are asked for the largest 3 elements i.e., k = 3
+then your program should print 50, 30 and 23.
+
+Time complexity : n * k
+ */
+public class Approach4UsingMinHeap {
+    public static void main(String[] args) {
+        //int[] array = {6, 9, 1, 7, 2, 4, 3, 5, 0, 8};
+        //int[] array = {5, 1, 7, 2, 0, 4, 3, 9};
+        int[] array = {5, 1, 7, 2, 0, 4, 3};
+        int k = 4;
+        printKLargestNumbers(array, k);
+    }
+
+    private static void printKLargestNumbers(int[] array, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int i = 0; i < k; i++) {
+            minHeap.add(array[i]);
+        }
+        for (int i = k; i < array.length; i++) {
+            if (!minHeap.isEmpty() && minHeap.peek() <= array[i]) {
+                minHeap.poll();
+                minHeap.add(array[i]);
+            }
+        }
+        while (!minHeap.isEmpty()) {
+            System.out.println(minHeap.poll());
+        }
+    }
+}
