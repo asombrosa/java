d75d2c41997f3ee0daa5918eb6260ba42af644b7
Prerna Gandhi <prernagandhi97@gmail.com>
2021-02-16 14:24:43 +0530

    Microsoft Interview Question : renamed Approach1Using1Stack.java to Approach2Using1Stack.java

--- MicrosoftInterviewQuestions/src/com/interview/questions/sde2/kLargestElementInArray/Approach2Using1Stack.java
+++ MicrosoftInterviewQuestions/src/com/interview/questions/sde2/kLargestElementInArray/Approach2Using1Stack.java
@@ -7,7 +7,7 @@ For example, if given array is [1, 23, 12, 9, 30, 2, 50]
 and you are asked for the largest 3 elements i.e., k = 3
 then your program should print 50, 30 and 23.
  */
-public class Approach1Using1Stack {
+public class Approach2Using1Stack {
     public static void main(String[] args) {
         int[] array = {5, 9, 1, 7, 2, 0, 4, 3};
         int k = 4;
