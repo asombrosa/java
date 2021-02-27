e55d575f78b931e2832813e377864bb935d670c1
Prerna Gandhi <prernagandhi97@gmail.com>
2021-02-27 16:25:25 +0530

    Google Interview : Algo 1 : Insertion sort with comparator (Using item name -- sorting strings)

--- GooglePreparationAlgorithmsPart1/src/sorting/ComparatorUsingItemName.java
+++ GooglePreparationAlgorithmsPart1/src/sorting/ComparatorUsingItemName.java
@@ -1,16 +1,9 @@
 package sorting;
 
-import sorting.Item;
 
 public class ComparatorUsingItemName implements java.util.Comparator<Item> {
     @Override
     public int compare(Item item1, Item item2) {
-        if (item1.itemName.equals(item2.itemName)) {
-            return 0;
-        } else if (item1.itemName.length() < item2.itemName.length()) {
-            return -1;
-        } else {
-            return 1;
-        }
+        return item1.itemName.compareTo(item2.itemName);
     }
 }
