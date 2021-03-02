a43c549742e3c1ad80b5528d4d5ea5d2a8005dff
Prerna Gandhi <prernagandhi97@gmail.com>
2021-03-02 14:10:15 +0530

    Google Interview Preparation : Algo 2 : bfs

---
+++ GooglePreparationAlgorithmsPart1/src/ds/graphs/bfs/Runner.java
@@ -0,0 +1,20 @@
+package ds.graphs.bfs;
+
+import ds.graphs.Graph;
+
+public class Runner {
+    public static void main(String[] args) {
+        Graph g = new Graph(10);
+        g.addEdge(0, 5);
+        g.addEdge(2, 4);
+        g.addEdge(2, 3);
+        g.addEdge(1, 2);
+        g.addEdge(0, 1);
+        g.addEdge(3, 4);
+        g.addEdge(3, 5);
+        g.addEdge(0, 2);
+        g.print();
+        BreadthFirstSearchPaths dfs = new BreadthFirstSearchPaths(g, 0);
+
+    }
+}
