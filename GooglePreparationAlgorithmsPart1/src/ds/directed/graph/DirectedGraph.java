649cbed53863919ae04f163dff2c59f1c854f72e
Prerna Gandhi <prernagandhi97@gmail.com>
2021-03-02 15:43:08 +0530

    Google Interview Preparation : Algo 2 : Directed graph

---
+++ GooglePreparationAlgorithmsPart1/src/ds/directed/graph/DirectedGraph.java
@@ -0,0 +1,36 @@
+package ds.directed.graph;
+
+import ds.graphs.Bag;
+
+public class DirectedGraph {
+    Bag<Integer> adj[];
+    int v;
+
+    DirectedGraph(int v) {
+        this.v = v;
+        adj = new Bag[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new Bag<>();
+        }
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    public int getVertices() {
+        return v;
+    }
+
+    public void print() {
+        for (int v = 0; v < 10; v++) {
+            for (int w : adj(v)) {
+                System.out.println(v + " => " + w);
+            }
+        }
+    }
+}
---
+++ GooglePreparationAlgorithmsPart1/src/ds/directed/graph/Runner.java
@@ -0,0 +1,20 @@
+package ds.directed.graph;
+
+import ds.graphs.Graph;
+
+public class Runner {
+    public static void main(String[] args) {
+        DirectedGraph g = new DirectedGraph(10);
+        g.addEdge(0,5);
+        g.addEdge(0,1);
+        g.addEdge(2,0);
+        g.addEdge(2,3);
+        g.addEdge(3,5);
+        g.addEdge(3,2);
+        g.addEdge(4,3);
+        g.addEdge(4,2);
+        g.addEdge(5,4);
+        g.print();
+
+    }
+}
